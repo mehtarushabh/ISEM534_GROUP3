6916e91473972b65c0fe0fdf50c431ebfc39f1a0
Rushabh Mehta <mehtarushabh143@outlook.com>
2023-05-28 18:40:00 -0400

    First

--- ALLQUERIES.sql
+++ ALLQUERIES.sql
@@ -310,3 +310,17 @@ VALUES (7, 'REGULAR', '2023-04-28', '07:30', 2, 5, 2);
 INSERT INTO appointment (appointment_id, appointment_type, appointment_date, appointment_time, patient_id, doctor_id,
                          subscription_id)
 VALUES (8, 'REGULAR', '2023-04-26', '07:30', 2, 5, 2);
+
+
+
+INSERT INTO drug_prescription (prescription_id, drug_name, quantity, dosage, status, appointment_id, patient_id,
+                               doctor_id)
+VALUES (1,'Tylenol',5,'200mg','Pending',4,1,2);
+
+INSERT INTO drug_prescription (prescription_id, drug_name, quantity, dosage, status, appointment_id, patient_id,
+                               doctor_id)
+VALUES (2,'Paxlovid',3,'200mg','Collected',8,2,5);
+
+
+INSERT INTO drug_collection (collection_id, cost, collection_date, paid, prescription_id, pharmacy_id, subscription_id)
+VALUES (1,'45.00','2023-05-28',TRUE,2,2,2);
